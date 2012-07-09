27ad4769528649ec03b1bed14e55b3afd5d4a395
Petrb <benapetr@gmail.com>
2012-07-09 21:19:23 +0200

    :)

--- Forms/Help.cs
+++ Forms/Help.cs
@@ -20,6 +20,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
@@ -35,7 +36,7 @@ namespace Client
 
         private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
         {
-
+            Process.Start(linkLabel1.Text);
         }
 
         private void Help_Load(object sender, EventArgs e)
