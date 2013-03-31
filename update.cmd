42b0e7a1aaace4bc4799c1e10ad1830e7b090aa5
Tanjoodo <tanjoodo@hotmail.com>
2013-03-31 22:30:27 +0300

    Fixed update.cmd to work on x86 machines as well

--- update.cmd
+++ update.cmd
@@ -1,7 +1,7 @@
 if defined ProgramFiles(x86) (
 PATH=%PATH%;%ProgramFiles(x86)%\Git\bin
 ) else (
-PATH = %PATH%;%ProgramFiles%\Git\bin
+PATH=%PATH%;%ProgramFiles%\Git\bin
 )
 cd %1
 git rev-list HEAD --count > %2
