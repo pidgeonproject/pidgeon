434881455f61a8839040544da356c1de042d0b4a
Petr Bena <benapetr@gmail.com>
2013-04-30 10:03:42 +0200

    comments

--- Forms/Preferences/Highlighter.cs
+++ Forms/Preferences/Highlighter.cs
@@ -36,6 +36,9 @@ namespace Client.Forms
 
         public ListStore list = new ListStore(typeof(string), typeof(Ignoring.Ignore.Type));
 
+		/// <summary>
+		/// Initializes a new instance of the <see cref="Client.Forms.Preferences_Hl"/> class.
+		/// </summary>
         public Preferences_Hl()
         {
             this.Build();
