c8c4892b7439abbd074f7a46f1ea80b7ba8dc2b7
petanb <petanb@linux-dev>
2013-05-03 11:27:59 +0200

    mods

--- buildall.sh
+++ buildall.sh
@@ -5,7 +5,7 @@ mkdir "bin/Debug/modules"
 original_path=`pwd`
 echo "Building tab completion"
 cd "modules/pidgeon_tc/pidgeon_tab/pidgeon_tab/"
-xbuild && mv bin/Debug/pidgeon_tc.dll "$original_path/bin/Debug/modules/pidgeon_tc.pmod"
+xbuild && mv bin/Debug/pidgeon_tab.dll "$original_path/bin/Debug/modules/pidgeon_tc.pmod"
 cd "$original_path"
 echo "Creating network data"
 cd "modules/NetworkInfo"
