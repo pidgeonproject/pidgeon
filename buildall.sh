9ed84275ee1e50463d4baf7ec4c77b26bd1a6351
petanb <petanb@linux-dev>
2013-05-03 11:24:41 +0200

    bugs

--- buildall.sh
+++ buildall.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 echo "Creating modules"
-mkdir bin/Debug/modules
+mkdir "bin/Debug/modules"
 original_path=`pwd`
 echo "Building tab completion"
-cd modules/pidgeon_tc/pidgeon_tab/pidgeon_tab/
-xbuild && mv bin/Debug/pidgeon_tc.dll $original_path/bin/Debug/modules/pidgeon_tc.pmod
-cd $original_path
+cd "modules/pidgeon_tc/pidgeon_tab/pidgeon_tab/"
+xbuild && mv bin/Debug/pidgeon_tc.dll "$original_path/bin/Debug/modules/pidgeon_tc.pmod"
+cd "$original_path"
 echo "Creating network data"
-cd modules/NetworkInfo 
-xbuild && mv bin/Debug/NetworkData.dll $original_path/bin/Debug/modules/networkdata.pmod
+cd "modules/NetworkInfo"
+xbuild && mv bin/Debug/NetworkInfo.dll "$original_path/bin/Debug/modules/networkdata.pmod"
