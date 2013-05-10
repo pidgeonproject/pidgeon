a49e572527d5ec919bc58b1992fd9a094a13e4a9
Petr Bena <benapetr@gmail.com>
2013-05-10 14:12:28 +0200

    extension Cloud

---
+++ modules/CloudStyle/CloudStyle/Class.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Client
+{
+    public class RestrictedModule : Extension
+    {
+        public override void Initialise()
+        {
+            Name = "CloudStyle";
+            Description = "This plugin changed the channel messages to look like in irc cloud";
+            Version = "1.0";
+            base.Initialise();
+        }
+
+        private void Message(Graphics.Window window, string text)
+        {
+            if (window == null)
+            {
+                return;
+            }
+
+            if (window.scrollback.IsEmtpy)
+            {
+                window.scrollback.InsertPart(text, ContentLine.MessageStyle.Join, false);
+            }
+            else
+            {
+                window.scrollback.InsertPart(", " + text, ContentLine.MessageStyle.Join, false);
+            }
+        }
+
+        public override bool Hook_UserJoin(Network network, User user, Channel channel)
+        {
+            Message(channel.retrieveWindow(), user.Nick + " joined");
+            return false;
+        }
+
+        public override bool Hook_UserPart(Network network, User user, Channel channel, string message)
+        {
+            Message(channel.retrieveWindow(), user.Nick + " parted");
+            return false;
+        }
+
+        public override bool Hook_UserQuit(Network network, User user, string message, Graphics.Window window)
+        {
+            Message(window, user.Nick + " joined");
+            return false;
+        }
+    }
+}
