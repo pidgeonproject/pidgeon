5cf821973a5004caf91cc7c27b153183d17a2209
Petr Bena <benapetr@gmail.com>
2013-07-13 16:06:16 +0200

    build system

--- build/maketar.sh
+++ build/maketar.sh
@@ -8,9 +8,41 @@ fi
 echo "Enter a version (for example 1.2.4.0)":
 read v
 
+temp=/tmp/pidgeon-$v.orig
+
 if [ -d /tmp/pidgeon-$v.orig ];then
-  echo "Remove /tmp/pidgeon-$v.orig before this"
+  echo "Remove $temp before this"
   exit 1
 fi
 
-mkdir /tmp/p
+mkdir $temp || exit 1
+
+echo "Copying all source codes"
+cp Pidgeon.csproj $temp
+cp *sln $temp
+cp *cs $temp
+cp -r modules $temp/modules
+cp -r Core $temp/Core
+cp -r Commands $temp/Commands
+cp -r Resources $temp/Resources
+cp -r Extensions $temp/Extensions
+cp -r WinForms $temp/WinForms
+cp -r Graphics $temp/Graphics
+cp -r Forms $temp/Forms
+cp -r ManualPages $temp/ManualPages
+cp -r Protocols $temp/Protocols
+cp -r Properties $temp/Properties
+cp -r Scrollback $temp/Scrollback
+cp -r RichTBox $temp/RichTBox
+cp -r skins $temp/skins
+echo "Copying makefiles"
+cp -r build $temp/build
+cp *sh $temp
+cp Makefile $temp
+
+echo "Creating a tarball"
+
+cd /tmp
+tar -zcf $temp.tar.gz pidgeon-$v.orig || exit 1
+
+echo "Done, your tarball is at $temp.tar.gz"
